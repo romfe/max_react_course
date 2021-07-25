0ae9b260d1002c6f3f19b00e5f39eef6ebb6bf50
Rômulo <romfe89@gmail.com>
2021-07-25 00:00:40 -0300

    added the no expenses message and verification

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
@@ -13,15 +13,12 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() == filteredYear;
   });
+
   return (
     <div>
-
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title}
-          amount={expense.amount}
-          date={expense.date} />)
-        }
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
